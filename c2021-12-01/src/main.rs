8fff0966d535ac96743524b1022f9f11ca6deffc
Mathis EON <eon@abes.fr>
2021-12-01 20:29:58 +0100

    2021-12-01 pt2

--- c2021-12-01/src/main.rs
+++ c2021-12-01/src/main.rs
@@ -1,14 +1,26 @@
 fn main() {
     let measurements = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-    let result = measurements.windows(2)
-        .fold(0, |mut counter, pair| {
-            if pair[1] > pair[0] {
-                counter += 1;
-            }
-            
-            counter
-        });
+    let result_part_1 = measurements.windows(2).fold(0, |mut counter, pair| {
+        if pair[1] > pair[0] {
+            counter += 1;
+        }
 
-    assert_eq!(result, 7);
+        counter
+    });
+
+    assert_eq!(result_part_1, 7);
+
+    let result_part_2 = measurements.windows(4).fold(0, |mut counter, pair| {
+        let p1_sum = pair[0] + pair[1] + pair[2];
+        let p2_sum = pair[1] + pair[2] + pair[3];
+
+        if p2_sum > p1_sum {
+            counter += 1;
+        }
+
+        counter
+    });
+
+    assert_eq!(result_part_2, 5);
 }
